b03387b55d5eaa0a2adc47cdd84d35fc79099439
SachPrecious <sachithrabandara49@gmail.com>
2022-04-09 20:22:44 +0530

    Initial Commit

---
+++ src/com/company/ClipBoard.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class ClipBoard {
+    private String content;
+
+    //private static ClipBoard instance;
+
+   /*
+    private ClipBoard{
+
+
+    }
+    */
+
+
+
+    public void copy(String c){
+        this.content=c;
+    }
+
+    public void paste(){
+        System.out.println(content);
+
+    }
+
+
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        ClipBoard clipBoard1=new ClipBoard();
+        ClipBoard clipBoard2=new ClipBoard();
+
+        clipBoard1.copy("abc");
+        clipBoard1.paste();
+
+
+
+    }
+}
