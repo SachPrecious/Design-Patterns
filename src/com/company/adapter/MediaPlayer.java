034803b76f61e32dc80c4d8eecc5bb84a5ec1a51
SachPrecious <sachithrabandara49@gmail.com>
2022-04-25 08:23:23 +0530

    Edited

--- src/com/company/adapter/MediaPlayer.java
+++ src/com/company/adapter/MediaPlayer.java
@@ -5,5 +5,18 @@ public class MediaPlayer {
     //depending on the file type play the file using a player
     public static void main(String[] args) {
 
+        String fileName="F:\\SENG\\file.mp3";
+        Player player=null;
+        if (fileName.endsWith("mp3")){
+            player=new Mp3Player();
+        }
+        else if (fileName.endsWith("mp4")){
+            player=new Mp4Player();
+        }
+
+        player.play(fileName);
+
+
+
     }
 }
