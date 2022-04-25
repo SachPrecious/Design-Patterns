8678a5bf68f62fb32fc9f143af14a71e7e91c4d8
SachPrecious <sachithrabandara49@gmail.com>
2022-04-25 08:17:23 +0530

    Edited

--- src/com/company/adapter/MediaPlayer.java
+++ src/com/company/adapter/MediaPlayer.java
@@ -1,5 +1,9 @@
 package com.company.adapter;
 
 public class MediaPlayer {
+    //get the file name from the user
+    //depending on the file type play the file using a player
+    public static void main(String[] args) {
 
+    }
 }
